108156d9280837fd4d9bce936b5dfb6f2bbae90a
Salvatore Ranieri <salvatore.ranieri@sysdata.it>
2021-11-21 10:38:00 +0100

    added splash api

--- app/src/main/java/com/sample/pokedex/PokedexActivity.kt
+++ app/src/main/java/com/sample/pokedex/PokedexActivity.kt
@@ -2,6 +2,7 @@ package com.sample.pokedex
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.appbar.AppBarLayout
 import com.sample.pokedex.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,7 @@ class PokedexActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
